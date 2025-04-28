10f915d85cf419d0889b7a120fe8929671e60290
romanvolkonidov <romanvolkonidov@gmail.com>
2025-04-28 13:07:56 +0300

    hh

--- server/index.js
+++ server/index.js
@@ -9,10 +9,10 @@ const multer = require('multer');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const FRONTEND_URL = process.env.FRONTEND_URL; // Add this line
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
 app.use(cors({
-  origin: FRONTEND_URL, // Use the environment variable
+  origin: FRONTEND_URL,
   credentials: true,
 }));
 app.use(express.json());
