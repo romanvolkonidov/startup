18d6f75fd2feafe59c39d4a505f270577120817f
romanvolkonidov <romanvolkonidov@gmail.com>
2025-04-28 11:55:54 +0300

    d

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import { NotificationProvider } from './context/NotificationContext';
 import { JobProvider } from './context/JobContext';
 
 const container = document.getElementById('root');
-const root = createRoot(container!);
+
+if (!container) {
+  throw new Error('Root container not found.');
+}
+
+const root = createRoot(container);
+
 root.render(
-  <AuthProvider>
-    <NotificationProvider>
-      <JobProvider>
-        <App />
-      </JobProvider>
-    </NotificationProvider>
-  </AuthProvider>
+  <React.StrictMode>
+    <AuthProvider>
+      <NotificationProvider>
+        <JobProvider>
+          <App />
+        </JobProvider>
+      </NotificationProvider>
+    </AuthProvider>
+  </React.StrictMode>
 );
